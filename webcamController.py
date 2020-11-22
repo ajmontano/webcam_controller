1d2d79202be9dfa1ec4e8641a61878b95c22d612
ajmontano <69804911+ajmontano@users.noreply.github.com>
2020-11-21 17:41:59 -0700

    Webcam GUI
    Attempt to create a GUI to display the webcam image.

--- webcamController.py
+++ webcamController.py
@@ -1,17 +1,51 @@
 import numpy as np
+import tkinter as tk
+from PIL import Image, ImageTk
 import cv2
 
+# Define the dimensions, in pixels, of a full screen
+width = 1920
+height = 1080
+
+# Define the focus level of the camera
+focus = 0
+FOCUS = 5
+FOCUS_MIN = 0
+FOCUS_MAX = 255
+
 cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
-while(True):
+# Change the focus
+cap.set(cv2.CAP_PROP_FOCUS, focus) 
+
+root = tk.Tk()
+
+# Setup the image frame
+imageFrame = tk.Frame(root, width=width, height=height)
+imageFrame.grid(row=0, column=0, padx=10, pady=10)
+
+lmain = tk.Label(root)
+lmain.grid(row=0, column=0)
+
+def show_frame():
     # Capture frame-by-frame
-    ret, frame = cap.read()
+    retVal, frame = cap.read()
 
-    # Display the resulting frame
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    # Convert the frame into a an ImageTk
+    cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+    img = Image.fromarray(cv2image)
+    imageTk = ImageTk.PhotoImage(image=img)
+    
+    # Display the window
+    lmain.imgtk = imageTk
+    lmain.configure(image=imageTk)
+    lmain.after(10, show_frame)
+
+# Run the display loop
+show_frame()
+root.mainloop()
 
 # When finished, release the capture
 cap.release()
-cv2.destroyAllWindows()
